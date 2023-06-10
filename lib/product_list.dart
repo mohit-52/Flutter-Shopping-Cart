1ec3a0818c1a9e738185904c96e72cd6e59e80d6
Mohit <mohitkaushis7@gmail.com>
2023-06-10 10:35:20 +0530

    Product List UI Added

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -8,21 +8,130 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Cherry',
+    'Peach',
+    'Mix Fruit Basket'
+  ];
+  List<String> productUnit = [
+    'KG',
+    'Dozen',
+    'KG',
+    'Dozen',
+    'KG',
+    'Dozen',
+    'KG',
+  ];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    "https://images.pexels.com/photos/7788414/pexels-photo-7788414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/161559/background-bitter-breakfast-bright-161559.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/60021/grapes-wine-fruit-vines-60021.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/61127/pexels-photo-61127.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/109274/pexels-photo-109274.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2253534/pexels-photo-2253534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/709567/pexels-photo-709567.jpeg?auto=compress&cs=tinysrgb&w=600"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Product List"),
+        title: const Text("Product List"),
         centerTitle: true,
-        actions: [
-         Center(
-           child: Badge(
-             label: Text('0'),
-             child: Icon(Icons.shopping_bag),
-           ),
-         ),
-
-          SizedBox(width: 20,)
+        actions: const [
+          Center(
+            child: Badge(
+              label: Text('0'),
+              child: Icon(Icons.shopping_bag),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: productName.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Row(
+                          children: [
+                            Image(
+                              height: 100,
+                              width: 100,
+                              image:
+                                  NetworkImage(productImage[index].toString()),
+                              fit: BoxFit.fill,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    productName[index].toString(),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "${productUnit[index]} \$${productPrice[index]}",
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Container(
+                                      height: 35,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          color: Colors.green,
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: const Center(
+                                        child: Text(
+                                          "Add to Cart",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ));
+                }),
+          )
         ],
       ),
     );
